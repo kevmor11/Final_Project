26ec81c660e144c6c989bb8dbbf27fe01a3069d9
Shawn Yates <yates.shawnp@gmail.com>
2017-05-20 23:25:35 +0000

    added validations for models

--- api_server/app/models/user.rb
+++ api_server/app/models/user.rb
@@ -6,15 +6,15 @@ class User < ApplicationRecord
   has_many :posts
   
   validates :first_name, presence: true
-  # validates :last_name, presence: true
-  # validates_length_of :password, minimum: 6
-  # EMAIL_REGEX = /\A\S+@.+\.\S+\z/
-  # USERNAME_REGEX = /\A[a-zA-Z0-9_-]{3,30}\z/
-  # validates :email, presence: true, uniqueness: { case_sensitive: false }, 
-  #                   format: { with: EMAIL_REGEX }
-  # validates :username, presence: true, uniqueness: { case_sensitive: false }, 
-  #                      format: { with: USERNAME_REGEX }
-  # mount_uploader :avatar, AvatarUploader
+  validates :last_name, presence: true
+  validates_length_of :password, minimum: 6
+  EMAIL_REGEX = /\A\S+@.+\.\S+\z/
+  USERNAME_REGEX = /\A[a-zA-Z0-9_-]{3,30}\z/
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, 
+                    format: { with: EMAIL_REGEX }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+                       format: { with: USERNAME_REGEX }
+  mount_uploader :avatar, AvatarUploader
 
   def self.authenticate_with_credentials(email, password)
     user = User.where("lower(email) = ?", email.downcase.strip).first
