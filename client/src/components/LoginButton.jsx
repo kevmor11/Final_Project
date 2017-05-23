f1356812018572cc40d0771c5c451dfe21e15dd5
nawartamawi <nawartamawi@gmail.com>
2017-05-22 21:30:03 -0700

    conditional rendering

---
+++ client/src/components/LoginButton.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default
+class LoginButton extends Component { 
+  render(){
+    return (
+      <p className="control">
+        <button className="button is-primary">Login</button>
+      </p>
+    )
+  }
+}
---
+++ client/src/components/RegistrationButton.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default
+class RegistrationButton extends Component { 
+  render(){
+    return (
+      <p className="control">
+        <button className="button is-primary">Registration</button>
+      </p>
+    )
+  }
+}
