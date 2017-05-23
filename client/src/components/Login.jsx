0edfd72eb92f8e75fc9871691ea2c2503411372d
nawartamawi <nawartamawi@gmail.com>
2017-05-22 18:31:51 -0700

    created First page components

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import Registration from './Registration.jsx'
+export default
+class Login extends Component { 
+  render(){
+    return (
+    <div className="login-registration container">
+        <div className="col-lg-6">
+          <form action="route" method="POST" className="login">
+            <h1 className="title">Login here</h1>
+            <h2 className="subtitle">Enter your Email and Password </h2>
+            <div className="field">
+              <label htmlFor="name" className="label">Email</label>
+              <p className="control">
+                <input className="input" type="email" id="name" placeholder="Email" />
+              </p>
+            </div>
+            <div className="field">
+              <label htmlFor="password" className="label">Password</label>
+              <p className="control">
+                <input className="input" type="password" id="password" placeholder="Password"/>
+              </p>
+            </div>
+            <div className="field is-grouped">
+              <p className="control">
+                <button className="button is-primary">Login</button>
+              </p>
+            </div>
+          </form>
+        </div>
+        <Registration />
+      </div> 
+    )
+  }
+}
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+import NavbarLeft from './NavbarLeft.jsx'
+class Navbar extends Component { 
+  render() {
+    return (
+      <div>
+        <nav className="nav has-shadow">
+          <div className="container">
+            <NavbarLeft />
+            <span className="nav-toggle">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+            <div className="nav-right nav-menu">
+              <a className="nav-item is-tab is-hidden-tablet is-active">Home</a>
+              <a className="nav-item is-tab is-hidden-tablet">Features</a>
+              <a className="nav-item is-tab is-hidden-tablet">Pricing</a>
+              <a className="nav-item is-tab is-hidden-tablet">About</a>
+              <a className="nav-item is-tab">
+                <figure className="image is-16x16" style={{"marginRight": "8px"}}>
+                  <img src="http://bulma.io/images/jgthms.png"/>
+                </figure>
+                Profile
+              </a>
+              <a className="nav-item is-tab">Log out</a>
+            </div>
+          </div>
+        </nav>
+      </div>
+    )
+  }
+}
+export default Navbar;
+
