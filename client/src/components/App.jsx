0378bb8753801c7ce7b9ce671448d5c21d3226a0
Jinny Huang <jinny921@gmail.com>
2017-05-22 17:59:04 -0700

    separated pinboard into components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,25 +1,26 @@
 import React, {Component} from 'react';
+import Navbar from './Navbar.jsx';
 
 export default
 class App extends Component { 
   render(){
     return (
     <div>
-      
-
+    <Navbar />
+    
       <div className="login-registration container">
         <div className="col-lg-6">
           <form action="route" method="POST" className="login">
             <h1 className="title">Login here</h1>
             <h2 className="subtitle">Enter your Email and Password </h2>
             <div className="field">
-              <label for="name" className="label">Email</label>
+              <label htmlFor="name" className="label">Email</label>
               <p className="control">
                 <input className="input" type="email" id="name" placeholder="Email" />
               </p>
             </div>
             <div className="field">
-              <label for="password" className="label">Password</label>
+              <label htmlFor="password" className="label">Password</label>
               <p className="control">
                 <input className="input" type="password" id="password" placeholder="Password"/>
               </p>
@@ -36,25 +37,25 @@ class App extends Component {
             <h1 className="title">Sign Up here</h1>
             <h2 className="subtitle">Join in and bridge with your loved ones </h2>
             <div className="field">
-              <label for="first-name" className="label">Firstname</label>
+              <label htmlFor="first-name" className="label">Firstname</label>
               <p className="control">
                 <input className="input" type="text" id="first-name" placeholder="First Name"/>
               </p>
             </div>
             <div className="field">
-              <label for="last-name" className="label">Last Name</label>
+              <label htmlFor="last-name" className="label">Last Name</label>
               <p className="control">
                 <input className="input" type="text" id="last-name" placeholder="Last Name"/>
               </p>
             </div>
             <div className="field">
-              <label for="email" className="label">Email</label>
+              <label htmlFor="email" className="label">Email</label>
               <p className="control">
                 <input className="input" type="email" id="email" placeholder="Email"/>
               </p>
             </div>
             <div className="field">
-              <label for="password" className="label">Password</label>
+              <label htmlFor="password" className="label">Password</label>
               <p className="control">
                 <input className="input" type="password" id="password" placeholder="Password"/>
               </p>
