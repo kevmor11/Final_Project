dae5e205547ded337773f47f3a7e394d5504078e
Shawn Yates <yates.shawnp@gmail.com>
2017-05-24 01:11:15 +0000

    configuring rails folder to include react

---
+++ api_server/app/assets/js/components/App.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Navbar from './Navbar.jsx';
+import LoginButton from './LoginButton.jsx';
+import RegistrationButton from './RegistrationButton.jsx';
+import LoginField from './LoginField.jsx';
+import RegistrationFields from './RegistrationFields.jsx'
+export default
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick () {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick () {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    let userFieldInput = null;
+    if (isLoggedIn) {
+      userFieldInput = <LoginField />;
+    } else {
+      userFieldInput = <RegistrationFields />;
+    }
+
+    return (
+      <div>
+
+        <Navbar/>
+
+        <div className="field is-grouped welcome-space">
+          <div className="col-log-2 optionButtons">
+            <LoginButton clickHandler={this.handleLoginClick}/>
+            <RegistrationButton clickHandler={this.handleLogoutClick} />
+          </div>
+          {userFieldInput}
+        </div>
+        
+      </div>
+    )
+  }
+}  
---
+++ api_server/app/assets/js/components/index.jsx
@@ -0,0 +1,7 @@
+// require("../styles/home.css");
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('react-root'));
