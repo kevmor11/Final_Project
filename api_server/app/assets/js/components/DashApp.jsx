6b44774cd0162454afdb437250e31a995ed41dea
nawartamawi <nawartamawi@gmail.com>
2017-05-24 17:38:33 -0700

    added notification, room UserProfile

--- api_server/app/assets/js/components/DashApp.jsx
+++ api_server/app/assets/js/components/DashApp.jsx
@@ -1,9 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import Navbar from './Navbar.jsx';
-import LoginButton from './LoginButton.jsx';
-import RegistrationButton from './RegistrationButton.jsx';
-import LoginField from './LoginField.jsx';
-import RegistrationFields from './RegistrationFields.jsx';
+import Notification from './Notification.jsx';
+import Rooms from './Rooms.jsx';
 export default
 class DashApp extends Component {
   static propTypes = {
@@ -19,13 +17,41 @@ class DashApp extends Component {
     return (
       <div>
         <Navbar />
-        
+        <div className="tile is-ancestor logged">
+          <div className="tile is-vertical is-8">
+            <div className="tile">
+              <div className="tile is-parent is-vertical">
+                <div className="notification notification-center">
+                  <p>Notification</p>
+                </div>
+                <Notification />
+              </div>
+              <div className="tile is-parent is-2">
+                <article className="tile is-child box dashboard profilebox">
+                  <p className="title">User Dashboard</p>
+                  <p className="subtitle">With an image</p>
+                  <figure className="image is-4by3">
+                    <img src="http://www.stottpilates.com/studio/images/instructors/person-placeholder.png"/>
+                  </figure>
+                  <a className="button is-primary">
+                    <span className="icon">
+                        <i className="fa fa-user-circle-o" aria-hidden="true"></i>
+                      </span>
+                    <span>Profile Setting</span>
+                  </a>
+
+                </article>
+              </div>
+            </div>
+          </div>
+          <Rooms/>
+        </div>
       </div>
     );
   }
 }
 
-// class ExtendedApp extends App {
+// className ExtendedApp extends App {
 //   render() {
 //     const inner = super.render(); // calls render in <App />
 //     return <div id="wrapped">{inner}</div>;
---
+++ api_server/app/assets/js/components/UserProfile.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import http from 'http';
+
+export default
+class Notification extends Component {
+
+  render() {
+    return (
+      <div className="notification">
+                  <img className="notification-avatar" src="http://www.stottpilates.com/studio/images/instructors/person-placeholder.png"/>
+                  <button className="delete"></button>
+                  <p className="notification-content">Nawar invited you to join his room.</p>
+                  <button className="btn btn-primary">Join</button>
+      </div>
+    )
+  }
+}  
