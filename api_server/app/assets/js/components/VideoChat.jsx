5b44af747ee4df84a5df94f608f0ef8bb62a22f6
kevmor11 <morissette.kevin@gmail.com>
2017-05-25 18:08:43 -0700

    linting

--- api_server/app/assets/js/components/VideoChat.jsx
+++ api_server/app/assets/js/components/VideoChat.jsx
@@ -6,7 +6,7 @@ class VideoChat extends Component {
   render() {
     return (
       <div>
-
+        <iframe src="https://appear.in/your-room-name" width="800" height="640" frameborder="0"></iframe>
       </div>
     );
   }
