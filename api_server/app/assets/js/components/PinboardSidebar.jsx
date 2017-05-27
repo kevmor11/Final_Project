b7d23b7ddadf3b99efe0e15267cc436813351ad7
kevmor11 <morissette.kevin@gmail.com>
2017-05-27 14:55:05 -0700

    setup Pinboard sidebar

--- api_server/app/assets/js/components/PinboardSidebar.jsx
+++ api_server/app/assets/js/components/PinboardSidebar.jsx
@@ -5,6 +5,8 @@ class PinboardSidebar extends Component {
   render(){
     return(
       <div>
+        {/*TODO Display all the users from the userrooms table that match the current_room ID which we should pass in to Sidebar
+        as a prop from the other component that determines the current_room state */}
         <div className="tile is-parent is-2 rooms">
           <article className="tile is-child box">
             <div className="content">
