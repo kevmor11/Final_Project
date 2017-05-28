f01cbf519858cbbe0c8d04a2dd9827e73e6ef398
nawartamawi <nawartamawi@gmail.com>
2017-05-28 13:33:31 -0700

    DashApp cleaning

--- api_server/app/assets/js/components/DashApp.jsx
+++ api_server/app/assets/js/components/DashApp.jsx
@@ -17,13 +17,6 @@ class DashApp extends Component {
     }
   }
 
-  componentDidMount() {
-
-    console.log("Did mount", "and props now are", this.props.userData.data.user);
-    this.setState({
-      user: this.props.userData.data.user
-    });
-  }
 
   render() {
     let rooms = [];
