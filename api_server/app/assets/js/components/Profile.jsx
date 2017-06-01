c6d0e90bd47536bb537d484f74c54ee7f303caf7
Jinny Huang <jinny921@gmail.com>
2017-05-31 23:30:05 -0700

    changed profile css

--- api_server/app/assets/js/components/Profile.jsx
+++ api_server/app/assets/js/components/Profile.jsx
@@ -11,7 +11,7 @@ class Profile extends Component {
           <p className="profile-name">{this.props.currentUser.first_name} Profile</p>
           <img className="profile-avatar" src="http://www.stottpilates.com/studio/images/instructors/person-placeholder.png"/>
           <div className="profile-content">
-            <p>Name: {this.props.currentUser.frist_name} {this.props.currentUser.last_name}</p>
+            <p>Name: {this.props.currentUser.first_name} {this.props.currentUser.last_name}</p>
             <p>Email: {this.props.currentUser.email}</p>
             <p>Gender: {this.props.currentUser.gender}</p>
           </div>
