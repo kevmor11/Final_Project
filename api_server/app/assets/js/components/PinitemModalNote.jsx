07378e0789f70055e066e4bfaff535337b9d074c
nawartamawi <nawartamawi@gmail.com>
2017-05-31 17:02:18 -0700

    delete icon is have a circle padding

--- api_server/app/assets/js/components/PinitemModalNote.jsx
+++ api_server/app/assets/js/components/PinitemModalNote.jsx
@@ -32,7 +32,7 @@ constructor(props) {
   render() {
     return(
       <div>
-        <a><i className="fa fa-trash-o" onClick={this.onClickDeletePost}></i></a>
+        <a><i className="fa fa-trash-o deleteIcon" onClick={this.onClickDeletePost}></i></a>
         <div className="item-container" onClick={this.open}>
           <h4><b className="item">{this.props.title}</b></h4>
           <hr/>
